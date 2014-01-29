dd33d73a5401a42974bf26a8f2c428a6aa1efa39
Stefan Bodewig <stefan.bodewig@freenet.de>
2014-01-29 20:12:15 +0100

    tests for collision detection

--- src/de/samaflost/clj_snake/collision_detection.clj
+++ src/de/samaflost/clj_snake/collision_detection.clj
@@ -2,24 +2,31 @@
   (:use [de.samaflost.clj-snake.level
          :only [bottom-door door-is-open? top-door]]))
 
-(defmulti collide? (fn [probe target]
-                     (cond (find target :location) :has-location
-                           (find target :body) :has-body
-                           :else (:type target))))
+;;; General purpose collision detection used for detecting whether the
+;;; snake hist a wall, itself, the other snake or the ball, the ball
+;;; hist a wall, an apple is generated on top of a snake and so on
+
+(defn- location-in-collection? [location collection]
+  (contains? (set collection) location))
+
+(defmulti collide?
+  "Tests whether probe and target collide where probe must be a map
+   with a :location key - dispatches based on target"
+  (fn [probe target]
+    (cond (find target :location) :has-location
+          (find target :body) :has-body
+          :else (:type target))))
 
 (defmethod collide? :has-location [{probe-location :location}
                                    {target-location :location}]
   (= probe-location target-location))
 
-(defn location-in-collection [location collection]
-  (contains? (set collection) location))
-
 (defmethod collide? :has-body [{:keys [location]} {:keys [body]}]
-  (location-in-collection location body))
+  (location-in-collection? location body))
 
 (defmethod collide? :level [{:keys [location]} level]
   (letfn [(collides-with-closed-door [door]
             (and (= location door) (not (door-is-open? level door))))]
     (or
-     (location-in-collection location (:walls level))
+     (location-in-collection? location (:walls level))
      (some collides-with-closed-door [top-door bottom-door]))))
---
+++ test/de/samaflost/clj_snake/collision_detection_test.clj
@@ -0,0 +1,79 @@
+(ns de.samaflost.clj-snake.collision-detection-test
+  (:require [clojure.test :refer :all]
+            [de.samaflost.clj-snake.collision-detection :refer :all])
+  (:use [de.samaflost.clj-snake.level
+         :only [bottom-door door-is-open? top-door]]))
+
+(deftest colliding-with-location
+  (testing "collisions with something that has a location"
+    (is (collide? {:location [1 2]} {:location [1 2]}))
+    (is (not (collide? {:location [1 2]} {:location [0 2]})))
+    (is (not (collide? {:location [1 2]} {:location [1 3]})))
+    (is (not (collide? {:location [1 2]} {:location []})))))
+
+(deftest colliding-with-a-body
+  (testing "collisions with something that has a body"
+    (is (collide? {:location [1 2]}
+                  {:body [[0 0] [1 2] [0 0]]}))
+    (is (collide? {:location [1 2]}
+                  {:body (list [0 0] [1 2] [0 0])}))
+    (is (not (collide? {:location [1 2]}
+                       {:body [[0 0] [1 3] [0 2]]})))
+    (is (not (collide? {:location [1 2]} 
+                       {:body []})))))
+
+(deftest colliding-with-a-level
+  (testing "collisions with the wall"
+    (is (collide? {:location [1 2]}
+                  {:walls [[0 0] [1 2] [0 0]]
+                   :top-door :open
+                   :bottom-door :open
+                   :type :level}))
+    (is (collide? {:location [1 2]}
+                  {:walls (list [0 0] [1 2] [0 0])
+                   :top-door :open
+                   :bottom-door :open
+                   :type :level}))
+    (is (not (collide? {:location [1 2]}
+                       {:walls [[0 0] [1 3] [0 2]]
+                        :top-door :open
+                        :bottom-door :open
+                        :type :level})))
+    (is (not (collide? {:location [1 2]} 
+                       {:walls []
+                        :top-door :open
+                        :bottom-door :open
+                        :type :level}))))
+  (testing "collisions with the top door"
+    (is (collide? {:location top-door}
+                  {:walls [[0 0] [1 2] [0 0]]
+                   :top-door :closed
+                   :bottom-door :open
+                   :type :level}))
+    (is (not (collide? {:location top-door}
+                       {:walls [[0 0] [1 2] [0 0]]
+                        :top-door :open
+                        :bottom-door :open
+                        :type :level})))
+    (is (collide? {:location [1 2]}
+                  {:walls [[0 0] [1 2] [0 0]]
+                   :top-door :closed
+                   :bottom-door :open
+                   :type :level})))
+  (testing "collisions with the bottom door"
+    (is (collide? {:location bottom-door}
+                  {:walls [[0 0] [1 2] [0 0]]
+                   :top-door :open
+                   :bottom-door :closed
+                   :type :level}))
+    (is (not (collide? {:location bottom-door}
+                       {:walls [[0 0] [1 2] [0 0]]
+                        :top-door :open
+                        :bottom-door :open
+                        :type :level})))
+    (is (collide? {:location [1 2]}
+                  {:walls [[0 0] [1 2] [0 0]]
+                   :top-door :open
+                   :bottom-door :closed
+                   :type :level}))))
+
