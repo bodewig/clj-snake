c245c11be1dafc2a4db54677b312e5b2aa18519e
Stefan Bodewig <stefan.bodewig@freenet.de>
2014-01-29 20:30:55 +0100

    more complete tests for snake

--- src/de/samaflost/clj_snake/snake.clj
+++ src/de/samaflost/clj_snake/snake.clj
@@ -2,6 +2,8 @@
   (:import (java.awt Color))
   (:use [de.samaflost.clj-snake.config :only [board-size]]))
 
+;;; snake creation, movement and slicing
+
 (def dirs
   ^{:private true}
   {:up [0 -1] :down [0 1] :left [-1 0] :right [1 0]})
@@ -9,12 +11,13 @@
 (defn- new-head [snake]
   (vec (apply map + [(first (:body snake)) (get dirs (:direction snake))])))
 
-(defn new-snake [player?]
+(defn- is-perpendicular? [dir1 dir2]
+  (= 0 (reduce + (map * (dirs dir1) (dirs dir2)))))
 
-  ^{:doc "Creates a new snake that will start at the bottom for a
-          player or at the top for the program-controlled snake"}
-  {
-   :body (list [(/ (:width board-size) 2)
+(defn new-snake [player?]
+  "Creates a new snake that will start at the bottom for a player or
+  at the top for the program-controlled snake"
+  {:body (list [(/ (:width board-size) 2)
                 (if player? (dec (:height board-size)) 0)])
    :direction (if player? :up :down)
    :to-grow 4
@@ -23,30 +26,31 @@
    })
 
 (defn move [snake]
-  ^{:doc "Moves and potentially grows a snake"}
+  "Moves and potentially grows a snake"
   (let [body (:body snake)
         to-grow (:to-grow snake)]
     (assoc snake :body (cons (new-head snake)
                              (if (> to-grow 0) body (butlast body)))
           :to-grow (if (> to-grow 1) (dec to-grow) 0))))
 
-(defn is-perpendicular? [dir1 dir2]
-  (= 0 (reduce + (map * (dirs dir1) (dirs dir2)))))
-
 (defn change-direction [snake new-dir]
-  ^{:doc "changes the direction of the snake to new-dir"}
+  "changes the direction of the snake to new-dir if new-dir is
+   perpendicular to current direction, otherwise the current direction
+   is kept"
   (if (is-perpendicular? (:direction snake) new-dir)
     (assoc snake :direction new-dir)
     snake))
 
 (defn consume [snake apple]
-  ^{:doc "grows the snake as it eats an apple"}
+  "grows the snake as it eats an apple"
   (assoc snake :to-grow
          (+ (:to-grow snake) (/ (:remaining-nutrition apple) 25))))
 
 (defn head [snake]
+  "Returns the snake's head as a :location-map"
   {:location (first (:body snake))})
 
 (defn tail [snake]
+  "Returns the snake's tail (all but its head) as a :body-map"
   {:body (next (:body snake))})
 
--- test/de/samaflost/clj_snake/snake_test.clj
+++ test/de/samaflost/clj_snake/snake_test.clj
@@ -1,4 +1,5 @@
 (ns de.samaflost.clj-snake.snake-test
+  (:import (java.awt Color))
   (:require [clojure.test :refer :all]
             [de.samaflost.clj-snake.snake :refer :all]))
 
@@ -6,10 +7,12 @@
   (testing "Creating a new Snake"
     (testing "for the player"
       (is (= (list [25 49]) (:body (new-snake true))))
-      (is (= :up (:direction (new-snake true)))))
+      (is (= :up (:direction (new-snake true))))
+      (is (= Color/GREEN (:color (new-snake true)))))
     (testing "for the AI"
       (is (= (list [25 0]) (:body (new-snake false))))
-      (is (= :down (:direction (new-snake false)))))))
+      (is (= :down (:direction (new-snake false))))))
+      (is (= Color/BLUE (:color (new-snake false)))))
 
 (deftest moving-the-snake
   (testing "moving"
@@ -45,3 +48,40 @@
       (is (= 41 (:to-grow (move (assoc (new-snake true)
                                   :to-grow 42)))))))
 
+(deftest changing-direction
+  (testing "changing from :up"
+    (is (= :up (:direction (change-direction {:direction :up} :up))))
+    (is (= :left (:direction (change-direction {:direction :up} :left))))
+    (is (= :up (:direction (change-direction {:direction :up} :down))))
+    (is (= :right (:direction (change-direction {:direction :up} :right)))))
+  (testing "changing from :left"
+    (is (= :up (:direction (change-direction {:direction :left} :up))))
+    (is (= :left (:direction (change-direction {:direction :left} :left))))
+    (is (= :down (:direction (change-direction {:direction :left} :down))))
+    (is (= :left (:direction (change-direction {:direction :left} :right)))))
+  (testing "changing from :down"
+    (is (= :down (:direction (change-direction {:direction :down} :up))))
+    (is (= :left (:direction (change-direction {:direction :down} :left))))
+    (is (= :down (:direction (change-direction {:direction :down} :down))))
+    (is (= :right (:direction (change-direction {:direction :down} :right)))))
+  (testing "changing from :right"
+    (is (= :up (:direction (change-direction {:direction :right} :up))))
+    (is (= :right (:direction (change-direction {:direction :right} :left))))
+    (is (= :down (:direction (change-direction {:direction :right} :down))))
+    (is (= :right (:direction (change-direction {:direction :right} :right))))))
+
+(deftest consuming-apples
+  (testing "consume"
+    (is (= {:to-grow 13} (consume {:to-grow 12} {:remaining-nutrition 25})))))
+
+(deftest head-test
+  (testing "head"
+    (is (= {:location [1 2]} (head {:body [[1 2] [3 4] [5 6]]})))
+    (is (= {:location [1 2]} (head {:body [[1 2]]})))
+    (is (= {:location nil} (head {:body []})))))
+
+(deftest tail-test
+  (testing "tail"
+    (is (= {:body [[3 4] [5 6]]} (tail {:body [[1 2] [3 4] [5 6]]})))
+    (is (= {:body nil} (tail {:body [[1 2]]})))
+    (is (= {:body nil} (tail {:body []})))))
