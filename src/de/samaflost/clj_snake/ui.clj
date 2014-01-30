041bea9cea240d9f9941b6743fbdab20cff6071f
Stefan Bodewig <stefan.bodewig@freenet.de>
2014-01-30 09:27:52 +0100

    clean up ui ns

--- src/de/samaflost/clj_snake/ui.clj
+++ src/de/samaflost/clj_snake/ui.clj
@@ -4,41 +4,43 @@
            (java.awt.event KeyListener KeyEvent))
   (:require [de.samaflost.clj-snake.config
              :refer [board-size ms-per-turn pixel-per-point]]
-        [de.samaflost.clj-snake.level :refer [bottom-door top-door]]
+        [de.samaflost.clj-snake.level :refer [bottom-door top-door door-is-open?]]
         [de.samaflost.clj-snake.snake :refer [change-direction]]))
 
-(defmulti paint (fn [g item] (:type item)))
+;;; The Swing UI of the game
+
+(defmulti paint
+  "painting something (the second arg) on an AWT Graphics instance"
+  (fn [g item] (:type item)))
+
+(defn- scale-x [location]
+  (* (first location) pixel-per-point))
+
+(defn- scale-y [location]
+  (* (second location) pixel-per-point))
+
+(defn- paint-rect [g color location]
+  (.setColor g color)
+  (.fillRect g (scale-x location) (scale-y location)
+             pixel-per-point pixel-per-point))
 
 (defmethod paint :snake [g snake]
-  ^{:doc "paint the snake"}
   (doseq [pt (:body snake)]
-    (.setColor g (:color snake))
-    (.fillRect g
-               (* (first pt) pixel-per-point)
-               (* (second pt) pixel-per-point)
-               pixel-per-point pixel-per-point)))
+    (paint-rect g (:color snake) pt)))
 
 (defmethod paint :apple [g apple]
-  ^{:doc "paint the apple"}
   (let [loc (:location apple)]
     (.setColor g (:color apple))
-    (.fillOval g
-               (* (first loc) pixel-per-point)
-               (* (second loc) pixel-per-point)
+    (.fillOval g (scale-x loc) (scale-y loc)
                pixel-per-point pixel-per-point)))
 
 (defmethod paint :level [g level]
-  ^{:doc "paint the level walls"}
   (let [all-walls
         (concat (:walls level)
-                (when (= :closed (:top-door level)) (list top-door))
-                (when (= :closed (:bottom-door level)) (list bottom-door)))]
+                (filter (complement (partial door-is-open? level))
+                        [top-door bottom-door]))]
     (doseq [pt all-walls]
-      (.setColor g Color/GRAY)
-      (.fillRect g
-                 (* (first pt) pixel-per-point)
-                 (* (second pt) pixel-per-point)
-                 pixel-per-point pixel-per-point))))
+      (paint-rect g Color/GRAY pt))))
 
 (defn- create-panel [level snake apples]
   (proxy [JPanel] []
@@ -47,10 +49,8 @@
                   (* (:height board-size)  pixel-per-point)))
     (paintComponent [g]
       (proxy-super paintComponent g)
-      (paint g @level)
-      (paint g @snake)
-      (doseq [a @apples]
-        (paint g a)))))
+      (doseq [item (flatten [@level @snake @apples])]
+        (paint g item)))))
 
 (def key-code-to-direction
   ^{:private true}
@@ -62,8 +62,8 @@
    })
 
 (defn- change-snake-direction [snake key-code]
-  (let [new-dir (get key-code-to-direction key-code)]
-    (when new-dir (dosync (alter snake change-direction new-dir)))))
+  (when-let [new-dir (get key-code-to-direction key-code)]
+    (dosync (alter snake change-direction new-dir))))
 
 (defn ask-for-restart [frame title message]
   (=
