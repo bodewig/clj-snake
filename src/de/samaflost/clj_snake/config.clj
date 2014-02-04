1a1acbb310d58a5cd2aa123c0caeb5c33eb7259c
Stefan Bodewig <stefan.bodewig@freenet.de>
2014-02-04 18:37:12 +0100

    tweak config a little

--- src/de/samaflost/clj_snake/config.clj
+++ src/de/samaflost/clj_snake/config.clj
@@ -1,9 +1,9 @@
 ;; configuration of the snake Game
 (ns de.samaflost.clj-snake.config)
 
-(def board-size {:width 50 :height 50})
+(def board-size {:width 60 :height 60})
 (def ms-per-turn 100)
 (def pixel-per-point 10)
 (def number-of-apples 5)
-(def ms-to-escape 15000)
+(def ms-to-escape 12000)
 (def ai-strategy :short-sighted)
