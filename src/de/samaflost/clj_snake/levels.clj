a85967ac80dbd34ba1e8b62fb7990bdf7db768fe
Stefan Bodewig <stefan.bodewig@freenet.de>
2014-02-06 06:03:56 +0100

    selecting the level can be made a lot simpler

--- src/de/samaflost/clj_snake/levels.clj
+++ src/de/samaflost/clj_snake/levels.clj
@@ -9,37 +9,37 @@
 (def max-width
   (get-in @snake-configuration [:board-size :width]))
 
-(def number-of-levels 5)
+(def ^:private layouts
+  [(fn []
+     (list [(/ max-width 2) (/ max-height 2)]))
+   (fn []
+     (concat (map vector
+                  (range 4 (- max-width 5) 5)
+                  (range 4 (- max-height 5) 5))))
+   (fn []
+     (concat
+      (map (partial vector (/ max-width 6))
+           (range 6 (- max-height 7)))
+      (map (partial vector (* (/ max-width 6) 5))
+           (range 6 (- max-height 7)))))
+   (fn []
+     (concat
+      (map #(vector % (/ max-height 4))
+           (range 1 (/ max-width 8)))
+      (map #(vector % (/ max-height 2))
+           (range (* 7 (/ max-width 8)) max-width))
+      (map #(vector % (* (/ max-height 4) 3))
+           (range 1 (/ max-width 8)))))
+   (fn []
+     (concat
+      (map (partial vector (/ max-width 2))
+           (range 9 (- max-height 10)))
+      (map #(vector % (/ max-height 2))
+           (range 9 (- max-width 10)))))])
 
-(defmulti internal-walls (fn [number] (mod number number-of-levels)))
+(def number-of-levels (count layouts))
 
-(defmethod internal-walls 0 [number]
-  (list [(/ max-width 2) (/ max-height 2)]))
-
-(defmethod internal-walls 1 [number]
-  (concat (map vector
-               (range 4 (- max-width 5) 5)
-               (range 4 (- max-height 5) 5))))
-
-(defmethod internal-walls 2 [number]
-  (concat
-   (map (partial vector (/ max-width 6))
-        (range 6 (- max-height 7)))
-   (map (partial vector (* (/ max-width 6) 5))
-        (range 6 (- max-height 7)))))
-
-(defmethod internal-walls 3 [number]
-  (concat
-   (map #(vector % (/ max-height 4))
-        (range 1 (/ max-width 8)))
-   (map #(vector % (/ max-height 2))
-        (range (* 7 (/ max-width 8)) max-width))
-   (map #(vector % (* (/ max-height 4) 3))
-        (range 1 (/ max-width 8)))))
-
-(defmethod internal-walls 4 [number]
-  (concat
-   (map (partial vector (/ max-width 2))
-        (range 9 (- max-height 10)))
-   (map #(vector % (/ max-height 2))
-        (range 9 (- max-width 10)))))
+(defn internal-walls
+  "Produces the walls inside a level."
+  [number]
+  ((layouts (mod number number-of-levels))))
